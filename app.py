f3108594d6b8c251e01086149294a706c3ae1a1d
Tejas2901 <99545773+Tejas2901@users.noreply.github.com>
2022-07-24 12:23:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title = "tejas's streamlit app")
+
+st.subheader('Hi, I am Tejas')
+st.title('App to calculate sum of two numbers')
+
+A = st.number_input('Enter a number')
+B = st.number_input('Enter one more number')
+sum = A + B
+C = st.write('Sum of the given two number is ',sum)
